6dcbc3607703c037a9a8780003cbbb85fdd50882
Niels Buekers <niels.buekers@gmail.com>
2019-11-27 21:48:40 +0100

    basic start rental

--- contract/main.sol
+++ contract/main.sol
@@ -6,24 +6,66 @@ import "https://github.com/smartcontractkit/chainlink/evm/contracts/vendor/Ownab
 
 contract SunnyRental is ChainlinkClient, Ownable {
     
-    //public readable vars
-    int256 public avgtempF;
     
     //private vars
     uint256 constant private ORACLE_PAYMENT = 1*LINK/10; //payment is 0.100 but sol cannot work with decimals.
     address private honeycombWWOOracle = 0x4a3fbbb385b5efeb4bc84a25aaadcd644bd09721;
     string private honeycombWWOPastWeatherInt256JobId = "67c9353f7cc94102b750f84f32027217";
+    
+    struct RentalContract {
+        //the ID of the equipment being rented, i.e. surfboards or kites.
+        string equipmentId;
+        //address of the customer that initiates the rental contract
+        address customer;
+        //first day of the rental - inclusive
+        uint256 startDate;
+        //last day of the rental - inclusive
+        uint256 endDate;
+        //the average daily windspeed that is required for the contract to be 
+        uint256 dailyRequiredWindspeedKmph;
+        //the total amount to be paid after successful rental.
+        uint256 amount;
+        //the percentage of `amount` that is paid out as a service fee in case requirements are not met. `amount`-`serviceFeePct` is returned to the customer.
+        uint256 serviceFeePct;
+        //storing the retrieved windspeeds for the period of the rental
+        //key: date, value: windspeed.
+        mapping (uint256 => uint256) dailyWindSpeeds;
+        //storing wether or not windspeed has been retrieved for a specific day. Useful for when windspeed is 0 as uint256 defaults to 0, we would not know if it was retrieved as 0 or defaulting to 0.
+        //key: date, value: TRUE if it was retrieved. default FALSE when it hasn't been retrieved.
+        mapping (uint256 => bool) dailyWindSpeedsRetrieved;
+    }
+    
+     //maps equipmentId to his active rental contracts. Each equipment can only have one active contract at a time.
+    mapping(string => RentalContract) rentalContracts;
+    //maps the payout request id to the equipmentId it was requested for
+    mapping(bytes32 => string) payoutRequests;
 
-
-    //event definitions
-    event RequestWWODataFulfilled(bytes32 indexed requestId,int256 indexed data);
-        
     constructor() public Ownable() {
         setPublicChainlinkToken();
         setChainlinkOracle(honeycombWWOOracle);
     }
-    /* Public function to trigger fulfillment */
-    function updateWeather(string rentalContractId) public {
+
+    //event definitions
+    event RequestWWODataFulfilled(bytes32 indexed requestId,int256 indexed data);
+        
+    
+    
+    /**
+    ** Registers a new campaign
+    **
+    ** Param _rentalId
+    ** Param _startDate
+    ** Param _endDate
+    ** Param _minWindspeedKmph the minimum average daily windspeed required for the rental fee to be paid out.
+    ** Param _serviceFeePct the percentage of the rental amount the rental company still gets when conditions are not met.
+    **
+    ** Payable ensures the client can deposit ether that will be paid out to the rental company when the weatherconditions are met.
+    **/
+    function registerRentalContract(string _rentalId,uint256 _startDate,uint256 _endDate, uint256 _minWindspeedKmph,uint256 _serviceFeePct) public payable {
+        
+    }
+    
+    function requestSettlement(string equipmentId) public{
         //get the requirements for the contract : location & min windspeed
         
         //TODO
@@ -49,7 +91,6 @@ contract SunnyRental is ChainlinkClient, Ownable {
   */
   function fulfillWorldWeatherOnlineRequest(bytes32 _requestId, int256 _avgTempF) public recordChainlinkFulfillment(_requestId){
     emit RequestWWODataFulfilled(_requestId, _avgTempF);
-    avgtempF = _avgTempF;
   }
   
 /*
