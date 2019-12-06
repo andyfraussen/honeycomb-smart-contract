e5dcb72dd3fdc6b7654416c2ce43cc102ef6e641
Niels Buekers <niels.buekers@gmail.com>
2019-12-06 19:35:13 +0100

    todo fix timestamptodate

--- contract/main.sol
+++ contract/main.sol
@@ -4,7 +4,32 @@ import "https://github.com/smartcontractkit/chainlink/evm/contracts/ChainlinkCli
 import "https://github.com/smartcontractkit/chainlink/evm/contracts/vendor/Ownable.sol";
 import "https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary/blob/1ea8ef42b3d8db17b910b46e4f8c124b59d77c03/contracts/BokkyPooBahsDateTimeLibrary.sol";
 
-import { Strings } from "./strings.sol";
+library Strings {
+    function concat(string memory _base, string memory _value) internal pure returns (string) {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+        bytes memory _newValue = bytes(_tmpValue);
+
+        uint i;
+        uint j;
+
+        for(i=0; i<_baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
+        }
+
+        for(i=0; i<_valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i++];
+        }
+
+        return string(_newValue);
+    }
+    
+    function compareStrings (string memory a, string memory b) public pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+}
 
 contract SunnyRental is ChainlinkClient, Ownable {
     
