ec3674f7bdf6955c673e59ca97bea47c7b27e589
Andy Fraussen <andy@salamander.be>
2019-12-11 23:57:41 +0100

    correct adress contract

--- honeycomb/scripts/main.js
+++ honeycomb/scripts/main.js
@@ -8,8 +8,8 @@ availableButton.onclick = async () => {
         //window.web3 = new Web3(ethereum);
         try {
             await ethereum.enable();
-            let contract = web3.eth.contract(abi, '0xdB824df2788FF1Cb086773a94708e690EA555b91');
-            let contractInstance = contract.at('0xdB824df2788FF1Cb086773a94708e690EA555b91')
+            let contract = web3.eth.contract(abi);
+            let contractInstance = contract.at('0x40D8d66A03c1c2949837FffC241CE078AD0B15FA')
 
             let equipmentid = Math.floor(Math.random() * 101);
             let rentalId = Math.floor(Math.random() * 101);
@@ -20,7 +20,7 @@ availableButton.onclick = async () => {
             let serviceFee = 10000
 
             contractInstance.equipmentAvailable(equipmentid, date.toString(), (e, f) => {
-                if (f === false) {
+                if (f === true) {
                     alert('Date is still available! Continue to the last step to register the rental')
                 }
             })
@@ -30,12 +30,12 @@ availableButton.onclick = async () => {
             document.getElementById('LocationGroup').innerText = 'Location: ' + location
             document.getElementById('WindspeedGroup').innerText = 'Min windspeed: ' + windspeed
             document.getElementById('serviceFeeGroup').innerText = 'Servicefee: ' + serviceFee
-            let amount = 2111111111111
-            let gasValue = 21000
+            let amount = 211111111111111
+            let gasValue = 210000
 
             registerButton.onclick = function () {
                 contractInstance.registerRentalContract(equipmentid, rentalId, date.toString(), windspeed, location.toString(), serviceFee, '0xdB824df2788FF1Cb086773a94708e690EA555b91',
-                    {from: window.web3.currentProvider.selectedAddress, value: amount, gas: gasValue}, (err, call) => {
+                    {from: window.web3.currentProvider.selectedAddress, value: amount, gas: gasValue, gasPrice: 10000}, (err, call) => {
                         console.log(err, call)
                     })
             }
